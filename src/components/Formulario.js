ab27032cd00e9fb90a529f172d2aed93db8a7ed8
David Garcia <dgarciabravo13@gmail.com>
2020-07-30 19:53:45 +0200

    add form with handleChange

---
+++ src/components/Formulario.js
@@ -0,0 +1,65 @@
+import React, {useState} from "react";
+
+const Formulario = () => {
+
+  const [cita, setCita] = useState({
+    mascota:"",
+    propietario:"",
+    fecha:"",
+    hora:"",
+    sintomas:""
+  })
+
+  const handleChange = ()=>{
+    console.log("escribiendo...")
+  }
+  return (
+    <>
+      <h2>Crear Cita</h2>
+      <form>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={handleChange}
+        />
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño de la Mascota"
+          onChange={handleChange}
+        />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+        />
+        <label>Sintomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={handleChange}
+        ></textarea>
+        <button
+          type="submit" 
+          className="u-full-width button-primary"
+        >Agregar Cita</button>
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
